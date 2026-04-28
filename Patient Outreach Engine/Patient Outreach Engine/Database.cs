9563f2b48671647e1197c5ec1028a1fa09f2d718
flubber914 <117917092+flubber914@users.noreply.github.com>
2026-04-28 17:34:57 +0100

    database start and patient developed
    the patient is just a data entry for the database so to continue the database the patient must be finished

--- Patient Outreach Engine/Patient Outreach Engine/Database.cs
+++ Patient Outreach Engine/Patient Outreach Engine/Database.cs
@@ -6,5 +6,25 @@ namespace Patient_Outreach_Engine
 {
     internal class Database
     {
+        List<Patient> m_database = new List<Patient>();
+
+        /// <summary>
+        /// Constructor generates database on object creation as this program doesn't use a real database
+        /// </summary>
+        public Database()
+        {
+            GenerateDatabase();
+        }
+
+        /// <summary>
+        /// generates a preset database of patients including all the database patient information
+        /// </summary>
+        void GenerateDatabase()
+        {
+            m_database = new List<Patient>
+            {
+
+            };
+        }
     }
 }
--- Patient Outreach Engine/Patient Outreach Engine/Patient.cs
+++ Patient Outreach Engine/Patient Outreach Engine/Patient.cs
@@ -6,5 +6,26 @@ namespace Patient_Outreach_Engine
 {
     internal class Patient
     {
+        public enum RiskLevel { Low, Medium, High };
+        public enum PreferredContact { Mobile, Email, SMS, Mail, Private};
+        private string name;
+        private RiskLevel currentRisk;
+        private PreferredContact contactMethod;
+        private string lastNotes;
+
+        /// <summary>
+        /// this constructor creates a patient as a data entry into the database
+        /// </summary>
+        /// <param name="_name"></param>
+        /// <param name="_currentRisk">High risk patients may not benefit from a text from this system</param>
+        /// <param name="_contactMethod">patients preferred contact method, if private then they are treated the same as someone who is high risk</param>
+        /// <param name="_lastNotes">additional possible information about the patient</param>
+        public Patient(string _name, RiskLevel _currentRisk, PreferredContact _contactMethod, string _lastNotes)
+        {
+            name = _name;
+            currentRisk = _currentRisk;
+            contactMethod = _contactMethod;
+            lastNotes = _lastNotes;
+        }
     }
 }
